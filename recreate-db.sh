a65fd45e793ea088c9dd062aeff53779c377a085
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-01-26 12:16:48 +0100

    add more information to recreate databas

--- recreate-db.sh
+++ recreate-db.sh
@@ -2,6 +2,7 @@
 docker compose down;
 docker compose up -d;
 
+echo "Waiting for database to spin up"
 sleep 5;
 
 PGPASSWORD=some-secure-password psql -U exorcist -h 127.0.0.1 -p 5432 -d exorcist -f ./migration/database.sql;
