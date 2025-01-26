ad757f1d729e9c6eac2a629a3040f3533618faf4
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-01-26 12:18:07 +0100

    extracted the unmarshalling of probe data

--- internal/ffmpeg/probe.go
+++ internal/ffmpeg/probe.go
@@ -1,7 +1,10 @@
 package ffmpeg
 
 import (
+	"encoding/json"
 	"errors"
+
+	ffmpegGo "github.com/u2takey/ffmpeg-go"
 )
 
 type Stream struct {
@@ -18,6 +21,28 @@ type Format struct {
 type Probe struct {
 	Format  *Format  `json:"format"`
 	Streams []Stream `json:"streams"`
+}
+
+func UnmarshalProbeData(probeData string) (*Probe, error) {
+	var data *Probe
+	err := json.Unmarshal([]byte(probeData), &data)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func UnmarshalledProbe(path string) (*Probe, error) {
+	probeData, err := ffmpegGo.Probe(path)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := UnmarshalProbeData(probeData)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
 }
 
 func GetDimensions(streams []Stream) (width, height int, err error) {
--- internal/ffmpeg/probe_test.go
+++ internal/ffmpeg/probe_test.go
@@ -1,12 +1,17 @@
 package ffmpeg_test
 
 import (
+	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/slugger7/exorcist/internal/ffmpeg"
 )
 
-func TestNoVideoCodecInStreams_shouldCreateError(t *testing.T) {
+const testVideoPath = "./test_data/working_video.mp4"
+const brokenVideoPath = "./test_data/broken_video.mp4"
+
+func Test_GetDImensions_NoVideoCodecInStreams_shouldCreateError(t *testing.T) {
 	sterams := []ffmpeg.Stream{
 		{
 			CodecType: "not_video",
@@ -26,7 +31,7 @@ func TestNoVideoCodecInStreams_shouldCreateError(t *testing.T) {
 	}
 }
 
-func TestWithVideoCodec_shouldReturnHeightAndWidth_withNilError(t *testing.T) {
+func Test_GetDImensions_WithVideoCodec_shouldReturnHeightAndWidth_withNilError(t *testing.T) {
 	width, height := 69, 420
 	streams := []ffmpeg.Stream{
 		{
@@ -45,5 +50,107 @@ func TestWithVideoCodec_shouldReturnHeightAndWidth_withNilError(t *testing.T) {
 	}
 	if height != actualHeight {
 		t.Errorf("Actual height (%v) does not match expected height (%v)", actualHeight, height)
+	}
+}
+
+func Test_UnmarshalledProbe_WithBrokenTestVideoFile_ShouldThrowError(t *testing.T) {
+	_, err := ffmpeg.UnmarshalledProbe(brokenVideoPath)
+	if err != nil {
+		if !strings.Contains(err.Error(), "Invalid data found when processing input") {
+			t.Errorf("Incorrect error was thrown: %v", err)
+			return
+		} else {
+			return
+		}
+	}
+
+	t.Errorf("No error was thrown")
+}
+
+func Test_UnmarshalProbeData_WithAWorkingFile_ShouldCreateCorrectStruct(t *testing.T) {
+	actual, err := ffmpeg.UnmarshalledProbe(testVideoPath)
+	if err != nil {
+		t.Errorf("Error was thrown %v", err)
+		return
+	}
+
+	height, width := 270, 480
+	expectedFormat := ffmpeg.Format{
+		Duration: "33.023333",
+		Size:     "3889885",
+	}
+	expectedStream := ffmpeg.Stream{
+		Height:    &height,
+		Width:     &width,
+		CodecType: "video",
+	}
+
+	var actualVideoStream *ffmpeg.Stream
+	for _, v := range actual.Streams {
+		if v.CodecType == expectedStream.CodecType {
+			actualVideoStream = &v
+		}
+	}
+	if actualVideoStream == nil {
+		t.Error("Could not find a video stream")
+		return
+	}
+
+	if !reflect.DeepEqual(*actual.Format, expectedFormat) {
+		t.Error("Actual format does not match expected format")
+	}
+	if !reflect.DeepEqual(*actualVideoStream, expectedStream) {
+		t.Error("Actual video stream does not match expected video stream")
+	}
+}
+
+func Test_UnmarshalProbeData_WithInvalidJson_ShouldThrowError(t *testing.T) {
+	_, err := ffmpeg.UnmarshalProbeData("this is not json")
+	if err != nil {
+		if !strings.Contains(err.Error(), "invalid character 'h' in literal true (expecting 'r')") {
+			t.Errorf("Incorrect error was thrown: %v", err)
+			return
+		} else {
+			return
+		}
+	}
+	t.Error("No error was thrown")
+}
+
+func Test_UnmarshallProbeData_WithValidJson_ShouldParseJson(t *testing.T) {
+	jsonData := `{
+		"format": {
+			"duration": "66.6",
+			"size": "666"
+		},
+		"streams": [
+			{
+				"codec_type": "video",
+				"height": 69,
+				"width": 420
+			}
+		]
+	}`
+	expectedHeight, expectedWidth := 69, 420
+	expectedProbeData := ffmpeg.Probe{
+		Format: &ffmpeg.Format{
+			Duration: "66.6",
+			Size:     "666",
+		},
+		Streams: []ffmpeg.Stream{
+			{
+				CodecType: "video",
+				Height:    &expectedHeight,
+				Width:     &expectedWidth,
+			},
+		},
+	}
+	data, err := ffmpeg.UnmarshalProbeData(jsonData)
+	if err != nil {
+		t.Errorf("Could not unmarshal json data: %v", err)
+	}
+
+	if !reflect.DeepEqual(*data, expectedProbeData) {
+		t.Errorf("Expected data differed from actual data")
 	}
 }
