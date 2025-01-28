a0dd221384c757c1646fc8b9367820b6aed41075
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-01-28 12:33:35 +0100

    create job sql builder

--- internal/db/exorcist/public/table/table_use_schema.go
+++ internal/db/exorcist/public/table/table_use_schema.go
@@ -10,7 +10,9 @@ package table
 // UseSchema sets a new schema name for all generated table SQL builder types. It is recommended to invoke
 // this method only once at the beginning of the program.
 func UseSchema(schema string) {
+	Job = Job.FromSchema(schema)
 	Library = Library.FromSchema(schema)
 	LibraryPath = LibraryPath.FromSchema(schema)
+	SchemaMigrations = SchemaMigrations.FromSchema(schema)
 	Video = Video.FromSchema(schema)
 }
