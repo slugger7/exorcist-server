fbbaac1a492502ad9fecf7d707fb8502c1e658a1
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-01-28 11:59:47 +0100

    remove unnecessary trapping of execution of statements

--- internal/repository/library/library.go
+++ internal/repository/library/library.go
@@ -1,8 +1,6 @@
 package libraryRepository
 
 import (
-	"database/sql"
-
 	"github.com/go-jet/jet/v2/postgres"
 	"github.com/slugger7/exorcist/internal/db/exorcist/public/model"
 	"github.com/slugger7/exorcist/internal/db/exorcist/public/table"
@@ -21,13 +19,4 @@ func CreateLibraryStatement(name string) postgres.InsertStatement {
 	repository.DebugCheck(insertStatement)
 
 	return insertStatement
-}
-
-func QueryInsert(db *sql.DB, statement postgres.InsertStatement) (data []struct{ model.Library }, err error) {
-	err = statement.Query(db, &data)
-	if err != nil {
-		return nil, err
-	}
-
-	return data, nil
 }
