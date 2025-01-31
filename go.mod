d64e06beeb5926b2b8cb3efd2d54fffd0ebec88d
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-01-31 17:38:05 +0100

    authentication basics

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.23.4
 
 require (
 	github.com/gin-contrib/cors v1.7.3
+	github.com/gin-contrib/sessions v1.0.2
 	github.com/gin-gonic/gin v1.10.0
 	github.com/go-jet/jet/v2 v2.12.0
 	github.com/golang-migrate/migrate/v4 v4.18.2
@@ -26,6 +27,9 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.24.0 // indirect
 	github.com/goccy/go-json v0.10.4 // indirect
+	github.com/gorilla/context v1.1.2 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/gorilla/sessions v1.2.2 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
--- internal/server/routes.go
+++ internal/server/routes.go
@@ -2,11 +2,18 @@ package server
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 	"github.com/slugger7/exorcist/internal/job"
 )
+
+var secret = []byte("secret")
+
+const userKey = "user"
 
 func (s *Server) RegisterRoutes() http.Handler {
 	r := gin.Default()
@@ -18,6 +25,16 @@ func (s *Server) RegisterRoutes() http.Handler {
 		AllowCredentials: true, // Enable cookies/auth
 	}))
 
+	r.Use(sessions.Sessions("mysession", cookie.NewStore(secret)))
+
+	authenticated := r.Group("/api")
+	authenticated.Use(AuthRequired)
+	{
+		authenticated.GET("/users", s.getUsers)
+	}
+
+	r.POST("/login", s.login)
+	r.GET("/logout", s.logout)
 	r.GET("/", s.HelloWorldHandler)
 
 	r.GET("/health", s.healthHandler)
@@ -25,6 +42,65 @@ func (s *Server) RegisterRoutes() http.Handler {
 	r.GET("/library/scan", s.scanLibrary)
 
 	return r
+}
+
+func AuthRequired(c *gin.Context) {
+	session := sessions.Default(c)
+	user := session.Get(userKey)
+
+	if user == nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+		return
+	}
+
+	c.Next()
+}
+
+func (s *Server) login(c *gin.Context) {
+	session := sessions.Default(c)
+	username := c.PostForm("username")
+	password := c.PostForm("password")
+
+	if strings.Trim(username, " ") == "" || strings.Trim(password, " ") == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Parameters can't be empty"})
+		return
+	}
+
+	if username != "hello" || password != "world" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authentication failed"})
+		return
+	}
+
+	session.Set(userKey, username)
+	if err := session.Save(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save session"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Successfully authenticated user"})
+}
+
+func (s *Server) logout(c *gin.Context) {
+	session := sessions.Default(c)
+	user := session.Get(userKey)
+	if user == nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid session token"})
+		return
+	}
+
+	session.Delete(userKey)
+	if err := session.Save(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save session"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Successfully logged out"})
+}
+
+func (s *Server) getUsers(c *gin.Context) {
+	session := sessions.Default(c)
+	user := session.Get(userKey)
+	c.JSON(http.StatusOK, gin.H{"user": user})
 }
 
 func (s *Server) HelloWorldHandler(c *gin.Context) {
