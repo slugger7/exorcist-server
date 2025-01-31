8f1ef059cab786494f8b7726f9c2c551c334ff13
Kevin David Heritage <8116780+slugger7@users.noreply.github.com>
2025-01-31 09:03:02 +0100

    bye plantuml hello d2 (#3)

---
+++ scripts/generate-diagrams.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "Generating diagrams"
+
+SOURCE_DIR="./diagrams/src"
+DEST_DIR="./diagrams/out"
+
+if [ ! -d "$SOURCE_DIR" ]; then
+    echo "Source directory does not exist."
+    exit 1
+fi
+
+mkdir -p "$DEST_DIR"
+
+process_file() {
+    local source_file="$1"
+    local dest_file="$2"
+    
+    d2 "$source_file" "$dest_file"
+}
+
+find "$SOURCE_DIR" -type f -name "*.d2" | while read -r source_file; do
+    relative_path="${source_file#$SOURCE_DIR/}"
+    dest_file="${DEST_DIR}/${relative_path%.$SOURCE_EXT}.svg"
+
+    mkdir -p "$(dirname "$dest_file")"
+
+    process_file "$source_file" "$dest_file"
+done
+
+echo "Processing completed."
