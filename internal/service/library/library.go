9ae9ebd76d0e731e725463750bcb0af11cc10701
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-02-03 08:49:44 +0100

    create library service

---
+++ internal/service/library/library.go
@@ -0,0 +1,58 @@
+package libraryService
+
+import (
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/slugger7/exorcist/internal/db/exorcist/public/model"
+	"github.com/slugger7/exorcist/internal/environment"
+	"github.com/slugger7/exorcist/internal/repository"
+)
+
+type ILibraryService interface {
+	CreateLibrary(newLibrary *model.Library) error
+}
+
+type LibraryService struct {
+	Env  *environment.EnvironmentVariables
+	repo repository.IRepository
+}
+
+var libraryServiceInstance *LibraryService
+
+func New(repo repository.IRepository, env *environment.EnvironmentVariables) *LibraryService {
+	if libraryServiceInstance == nil {
+		libraryServiceInstance = &LibraryService{
+			Env:  env,
+			repo: repo,
+		}
+
+		log.Println("LibraryService instance created")
+	}
+	return libraryServiceInstance
+}
+
+func (i LibraryService) CreateLibrary(newLibrary *model.Library) error {
+	var libraries []struct {
+		model.Library
+	}
+	if err := i.repo.LibraryRepo().
+		GetLibraryByName(newLibrary.Name).
+		Query(&libraries); err != nil {
+		log.Printf("Could not fetch library by name %v", newLibrary.Name)
+		return err
+	}
+	if len(libraries) > 0 {
+		return errors.New(fmt.Sprintf("library named %v already exists", newLibrary.Name))
+	}
+
+	if err := i.repo.LibraryRepo().
+		CreateLibraryStatement(newLibrary.Name).
+		Query(&newLibrary); err != nil {
+		log.Printf("could not create library with name %v", newLibrary.Name)
+		return err
+	}
+
+	return nil
+}
