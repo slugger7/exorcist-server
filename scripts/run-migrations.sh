0985d2e8c1453b82c37b5d594dfae156b9ce7503
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-02-03 11:31:25 +0100

    fix scripts

--- scripts/run-migrations.sh
+++ scripts/run-migrations.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Running migrations"
 
-./scripts/set-env.sh
+. ./scripts/set-env.sh
 
 echo "Running migrations"
 migrate -source file://./migrations  -database "${DATABASE_CONNECTION_STRING}" -verbose up
