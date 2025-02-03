670ea59d4ee18728068cc9829a70d489da055c55
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-02-03 11:49:22 +0100

    create test for authentication

--- internal/server/authentication_test.go
+++ internal/server/authentication_test.go
@@ -3,14 +3,15 @@ package server
 import (
 	"bytes"
 	"errors"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/slugger7/exorcist/internal/db/exorcist/public/model"
 	libraryService "github.com/slugger7/exorcist/internal/service/library"
 	userService "github.com/slugger7/exorcist/internal/service/user"
@@ -20,10 +21,12 @@ type mockService struct {
 	userService userService.IUserService
 }
 
-type mockUserService struct{}
+type mockUserService struct {
+	validateUserModel model.User
+	validateUserError error
+}
 
 func (ms mockService) UserService() userService.IUserService {
-	fmt.Println("Return mock user service")
 	return ms.userService
 }
 func (ms mockService) LibraryService() libraryService.ILibraryService {
@@ -35,15 +38,23 @@ func (mus mockUserService) CreateUser(username, password string) (*model.User, e
 }
 
 func (mus mockUserService) ValidateUser(username, password string) (*model.User, error) {
-	return nil, errors.New("Testing error")
+	return &mus.validateUserModel, mus.validateUserError
 }
 
-func Test_Login(t *testing.T) {
+func Test_Login_Success(t *testing.T) {
 	s := &Server{}
 	r := gin.New()
 	r.Use(sessions.Sessions("exorcist", cookie.NewStore([]byte("cookieSecret"))))
 	r.POST("/", s.Login)
-	s.service = mockService{userService: mockUserService{}}
+
+	id, err := uuid.NewRandom()
+	if err != nil {
+		t.Fatalf("could not generate random uuid %v", err)
+	}
+
+	s.service = mockService{userService: mockUserService{
+		validateUserModel: model.User{Username: "admin", ID: id},
+	}}
 	body := []byte(`{"username": "admin", "password": "admin"}`)
 
 	req, err := http.NewRequest("POST", "/", bytes.NewReader(body))
@@ -56,5 +67,14 @@ func Test_Login(t *testing.T) {
 	r.ServeHTTP(rr, req)
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("Got an error %v %v", status, err)
+	}
+
+	// check to see if cookie was set
+	cookie := strings.Trim(rr.Header().Get("Set-Cookie"), " ")
+	if cookie == "" {
+		t.Errorf("No header is being set for exorcist")
+	}
+	if !strings.Contains(cookie, "exorcist") {
+		t.Errorf("cookie was not set up correctly: %v", cookie)
 	}
 }
