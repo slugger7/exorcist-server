04b3283683591d1de011c3f67aab2a7f75710600
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-02-12 19:03:29 +0100

    implement central mocks

--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -3,22 +3,17 @@ package service
 import (
 	"testing"
 
-	"github.com/google/uuid"
-	"github.com/slugger7/exorcist/internal/db/exorcist/public/model"
 	"github.com/slugger7/exorcist/internal/environment"
 	"github.com/slugger7/exorcist/internal/logger"
+	"github.com/slugger7/exorcist/internal/mocks/mservice"
 )
-
-type mockUserService struct{}
-type mockLibraryService struct{}
-type mockLibaryPathService struct{}
 
 var service = &Service{
 	env:                nil,
 	logger:             logger.New(&environment.EnvironmentVariables{LogLevel: "none"}),
-	userService:        mockUserService{},
-	libraryService:     mockLibraryService{},
-	libraryPathService: mockLibaryPathService{},
+	userService:        mservice.MockUserService{},
+	libraryService:     mservice.MockLibraryService{},
+	libraryPathService: mservice.MockLibaryPathService{},
 }
 
 func Test_UserService(t *testing.T) {
@@ -29,31 +24,15 @@ func Test_UserService(t *testing.T) {
 }
 
 func Test_LibraryService(t *testing.T) {
-	actualUserService := service.LibraryService()
-	if actualUserService == nil {
+	actualLibraryService := service.LibraryService()
+	if actualLibraryService == nil {
 		t.Error("library service was nil")
 	}
 }
 
-// unused mocks
-func (mus mockUserService) CreateUser(username, password string) (*model.User, error) {
-	panic("not implemented")
-}
-func (mus mockUserService) ValidateUser(username, password string) (*model.User, error) {
-	panic("not implemented")
-}
-func (mls mockLibraryService) CreateLibrary(_ model.Library) (*model.Library, error) {
-	panic("not implemented")
-}
-func (mls mockLibraryService) GetLibraries() ([]model.Library, error) {
-	panic("not implemented")
-}
-func (mls mockLibraryService) GetLibraryById(uuid.UUID) (*model.Library, error) {
-	panic("not implemented")
-}
-func (mlps mockLibaryPathService) Create(*model.LibraryPath) (*model.LibraryPath, error) {
-	panic("not implemented")
-}
-func (mlps mockLibaryPathService) GetAll() ([]model.LibraryPath, error) {
-	panic("not implemented")
+func Test_LibraryPathService(t *testing.T) {
+	actualLibraryPathService := service.LibraryPathService()
+	if actualLibraryPathService == nil {
+		t.Errorf("library path service was nil")
+	}
 }
