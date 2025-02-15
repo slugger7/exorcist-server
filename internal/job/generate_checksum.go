766a7cf8c0e61088d941eaa578a81ece0c3d725e
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-02-15 14:08:30 +0100

    comment out generate checksum method. (needs refactor any way)

--- internal/job/generate_checksum.go
+++ internal/job/generate_checksum.go
@@ -1,41 +1,32 @@
 package job
 
-import (
-	"log"
-	"path/filepath"
+// func GenerateChecksums(repo repository.IRepository) {
+// 	var data []struct {
+// 		model.LibraryPath
+// 		model.Video
+// 	}
+// 	err := repo.Video().
+// 		GetVideoWithoutChecksumStatement().
+// 		Query(&data)
+// 	if err != nil {
+// 		log.Printf("Error while fetching a video without a checksum %v", err.Error())
+// 	}
 
-	"github.com/slugger7/exorcist/internal/db/exorcist/public/model"
-	"github.com/slugger7/exorcist/internal/media"
-	"github.com/slugger7/exorcist/internal/repository"
-)
+// 	for _, v := range data {
+// 		absolutePath := filepath.Join(v.LibraryPath.Path, v.Video.RelativePath)
+// 		log.Printf("Calculating checksum for %v", v.Video.RelativePath)
+// 		checksum, err := media.CalculateMD5(absolutePath)
+// 		if err != nil {
+// 			log.Printf("Could not calculate checksum for %v. Video ID %v", absolutePath, v.Video.ID)
+// 		}
 
-func GenerateChecksums(repo repository.IRepository) {
-	var data []struct {
-		model.LibraryPath
-		model.Video
-	}
-	err := repo.Video().
-		GetVideoWithoutChecksumStatement().
-		Query(&data)
-	if err != nil {
-		log.Printf("Error while fetching a video without a checksum %v", err.Error())
-	}
+// 		v.Video.Checksum = &checksum
 
-	for _, v := range data {
-		absolutePath := filepath.Join(v.LibraryPath.Path, v.Video.RelativePath)
-		log.Printf("Calculating checksum for %v", v.Video.RelativePath)
-		checksum, err := media.CalculateMD5(absolutePath)
-		if err != nil {
-			log.Printf("Could not calculate checksum for %v. Video ID %v", absolutePath, v.Video.ID)
-		}
-
-		v.Video.Checksum = &checksum
-
-		_, err = repo.Video().UpdateVideoChecksum(v.Video).
-			Exec()
-		if err != nil {
-			log.Printf("Could not update the checksum of video (%v): %v", v.Video.ID, err)
-		}
-	}
-	log.Println("Completed checksum generation")
-}
+// 		_, err = repo.Video().UpdateVideoChecksum(v.Video).
+// 			Exec()
+// 		if err != nil {
+// 			log.Printf("Could not update the checksum of video (%v): %v", v.Video.ID, err)
+// 		}
+// 	}
+// 	log.Println("Completed checksum generation")
+// }
