784770304d5f629eb143478e9a4ef6a7c5f15355
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-02-19 13:01:10 +0100

    generate service mocks

--- scripts/generate-mocks.sh
+++ scripts/generate-mocks.sh
@@ -1,26 +1,48 @@
 #!/usr/bin/env bash
 echo "Generating mocks"
 
-MOCK_DIR="./internal/mock"
 REPO_DIR="./internal/repository"
+SERVICE_DIR="./internal/service"
+
+MOCK_DIR="./internal/mock"
+MOCK_REPO_DIR="${MOCK_DIR}/repository"
+MOCK_SERVICE_DIR="${MOCK_DIR}/service"
 
 mkdir -p ${MOCK_DIR}
 
-MOCK_REPO_DIR="${MOCK_DIR}/repository"
+echo "Generating repository mocks"
 mkdir -p ${MOCK_REPO_DIR}
-mockgen -source=${REPO_DIR}/repository.go > ${MOCK_DIR}/repository/repository.go
+mockgen -source=${REPO_DIR}/repository.go > ${MOCK_REPO_DIR}/repository.go
 
 mkdir -p ${MOCK_REPO_DIR}/job
-mockgen -source=${REPO_DIR}/job/job.go > ${MOCK_DIR}/repository/job/job.go
+mockgen -source=${REPO_DIR}/job/job.go >  ${MOCK_REPO_DIR}/job/job.go
 
 mkdir -p ${MOCK_REPO_DIR}/library
-mockgen -source=${REPO_DIR}/library/library.go > ${MOCK_DIR}/repository/library/library.go
+mockgen -source=${REPO_DIR}/library/library.go >  ${MOCK_REPO_DIR}/library/library.go
 
 mkdir -p ${MOCK_REPO_DIR}/library_path
-mockgen -source=${REPO_DIR}/library_path/library_path.go > ${MOCK_DIR}/repository/library_path/library_path.go
+mockgen -source=${REPO_DIR}/library_path/library_path.go >  ${MOCK_REPO_DIR}/library_path/library_path.go
 
 mkdir -p ${MOCK_REPO_DIR}/user
-mockgen -source=${REPO_DIR}/user/user.go > ${MOCK_DIR}/repository/user/user.go
+mockgen -source=${REPO_DIR}/user/user.go >  ${MOCK_REPO_DIR}/user/user.go
 
 mkdir -p ${MOCK_REPO_DIR}/video
-mockgen -source=${REPO_DIR}/video/video.go > ${MOCK_DIR}/repository/video/video.go
+mockgen -source=${REPO_DIR}/video/video.go >  ${MOCK_REPO_DIR}/video/video.go
+
+echo "Generate service mocks"
+mkdir -p ${MOCK_SERVICE_DIR}
+mockgen -source=${SERVICE_DIR}/service.go > ${MOCK_SERVICE_DIR}/service.go
+
+mkdir -p ${MOCK_SERVICE_DIR}/library
+mockgen -source=${SERVICE_DIR}/library/library.go > ${MOCK_SERVICE_DIR}/library/library.go
+
+mkdir -p ${MOCK_SERVICE_DIR}/library_path
+mockgen -source=${SERVICE_DIR}/library_path/library_path.go > ${MOCK_SERVICE_DIR}/library_path/library_path.go
+
+mkdir -p ${MOCK_SERVICE_DIR}/user
+mockgen -source=${SERVICE_DIR}/user/user.go > ${MOCK_SERVICE_DIR}/user/user.go
+
+mkdir -p ${MOCK_SERVICE_DIR}/video
+mockgen -source=${SERVICE_DIR}/video/video.go > ${MOCK_SERVICE_DIR}/video/video.go
+
+echo "Mocks generated"
