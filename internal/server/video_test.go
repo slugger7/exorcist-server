804dafc1f73e58cbe9931ed42e79ad626b95a2cb
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-02-21 08:57:17 +0100

    refactor tests

--- internal/server/video_test.go
+++ internal/server/video_test.go
@@ -8,9 +8,10 @@ import (
 	"github.com/google/uuid"
 	"github.com/slugger7/exorcist/internal/assert"
 	"github.com/slugger7/exorcist/internal/db/exorcist/public/model"
+	"go.uber.org/mock/gomock"
 )
 
-func Test_GetVideo_InvalidIdD(t *testing.T) {
+func Test_GetVideo_InvalidId(t *testing.T) {
 	s := setupServer(t)
 
 	id := "some invalid uuid"
@@ -24,63 +25,65 @@ func Test_GetVideo_InvalidIdD(t *testing.T) {
 }
 
 func Test_GetVideo_ServiceReturnsError(t *testing.T) {
-	s := setupOldServer()
+	s := setupServer(t).
+		withVideoService()
 
 	id, _ := uuid.NewRandom()
 
-	s.mockService.Video.MockError[0] = fmt.Errorf("error")
+	s.mockVideoService.EXPECT().
+		GetById(gomock.Eq(id)).
+		DoAndReturn(func(uuid.UUID) (*model.Video, error) {
+			return nil, fmt.Errorf("some error")
+		}).
+		Times(1)
 
 	rr := s.withGetEndpoint(s.server.GetVideo, ":id").
-		withGetRequest(nil, id.String()).
+		withGetRequest(id.String()).
 		exec()
 
-	expectedStatus := http.StatusInternalServerError
-	if expectedStatus != rr.Code {
-		t.Errorf("Expected status code: %v\nGot status code: %v", expectedStatus, rr.Code)
-	}
-
-	expectedBody := fmt.Sprintf(`{"error":"%v"}`, ErrGetVideoService)
-	if expectedBody != rr.Body.String() {
-		t.Errorf("Expected body: %v\nGot body: %v", expectedBody, rr.Body.String())
-	}
+	assert.StatusCode(t, http.StatusInternalServerError, rr.Code)
+	assert.Body(t, fmt.Sprintf(`{"error":"%v"}`, ErrGetVideoService), rr.Body.String())
 }
 
 func Test_GetVideo_VideoServiceNil(t *testing.T) {
-	s := setupOldServer()
+	s := setupServer(t).
+		withVideoService()
 
 	id, _ := uuid.NewRandom()
 
-	s.mockService.Video.MockModel[0] = nil
+	s.mockVideoService.EXPECT().
+		GetById(gomock.Eq(id)).
+		DoAndReturn(func(uuid.UUID) (*model.Video, error) {
+			return nil, nil
+		}).
+		Times(1)
 
 	rr := s.withGetEndpoint(s.server.GetVideo, ":id").
-		withGetRequest(nil, id.String()).
+		withGetRequest(id.String()).
 		exec()
 
-	expectedStatus := http.StatusNotFound
-	if expectedStatus != rr.Code {
-		t.Errorf("Expected status code: %v\nGot status code: %v", expectedStatus, rr.Code)
-	}
-
-	expectedBody := fmt.Sprintf(`{"error":"%v"}`, ErrVideoNotFound)
-	if expectedBody != rr.Body.String() {
-		t.Errorf("Expected body: %v\nGot body: %v", expectedBody, rr.Body.String())
-	}
+	assert.StatusCode(t, http.StatusNotFound, rr.Code)
+	assert.Body(t, fmt.Sprintf(`{"error":"%v"}`, ErrVideoNotFound), rr.Body.String())
 }
 
 func Test_GetVideo_Success(t *testing.T) {
-	s := setupOldServer()
+	s := setupServer(t).
+		withVideoService()
 
 	id, _ := uuid.NewRandom()
 	video := &model.Video{ID: id}
-	s.mockService.Video.MockModel[0] = video
+
+	s.mockVideoService.EXPECT().
+		GetById(gomock.Eq(id)).
+		DoAndReturn(func(uuid.UUID) (*model.Video, error) {
+			return video, nil
+		}).
+		Times(1)
 
 	rr := s.withGetEndpoint(s.server.GetVideo, ":id").
-		withGetRequest(nil, id.String()).
+		withGetRequest(id.String()).
 		exec()
 
-	expectedStatus := http.StatusMovedPermanently
-	if expectedStatus != rr.Code {
-		t.Errorf("Expected status code: %v\nGot status code: %v", expectedStatus, rr.Code)
-	}
+	assert.StatusCode(t, http.StatusMovedPermanently, rr.Code)
 	// TODO: figure out how to test that a video is returned
 }
