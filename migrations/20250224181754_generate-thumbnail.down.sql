94e12f4e84cd6495a576f45280fd53be47f4569a
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-02-25 07:32:34 +0100

    create migration for new job type

---
+++ migrations/20250224181754_generate-thumbnail.down.sql
@@ -0,0 +1,17 @@
+begin;
+
+  alter type job_type_enum rename to old_job_type_enum;
+
+  create type job_type_enum as enum ('update_existing_videos', 'scan_path','generate_checksum');
+
+  alter table job rename column job_type to old_job_type;
+
+  alter table job add job_type job_type_enum not null default 'scan_path';
+
+  update job set job_type = old_job_type::text::job_type_enum;
+
+  alter table job drop column old_job_type;
+
+  drop type old_job_type_enum;
+
+commit;
