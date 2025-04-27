017de745222c16b8d6cab7a0b8e9b9cf58274857
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-04-27 14:28:11 +0200

    modify checksum field length

--- migrations/20250127153345_initial_migration.up.sql
+++ migrations/20250127153345_initial_migration.up.sql
@@ -1,11 +1,13 @@
-create table library (
+create table library
+(
   id uuid primary key default gen_random_uuid(),
   "name" varchar not null unique,
   created timestamp default current_timestamp not null,
   modified timestamp default current_timestamp not null
 );
 
-create table library_path (
+create table library_path
+(
   id uuid primary key default gen_random_uuid(),
   library_id uuid not null,
   path varchar not null unique,
@@ -16,7 +18,8 @@ create table library_path (
     on delete cascade
 );
 
-create table video (
+create table video
+(
   id uuid primary key default gen_random_uuid(),
   library_path_id uuid not null,
   relative_path varchar not null,
@@ -26,30 +29,36 @@ create table video (
   width int not null,
   runtime double precision not null,
   size bigint not null,
-  checksum char(40),
+  checksum char(32),
   added timestamp default current_timestamp not null,
   deleted boolean default false not null,
   exists boolean default true not null,
   created timestamp default current_timestamp not null,
   modified timestamp default current_timestamp not null,
   constraint fk_video_library_path
-    foreign key(library_path_id) references library_path(id)
-    on delete cascade
+    foreign key
+  (library_path_id) references library_path
+  (id)
+    on
+  delete cascade
 );
 
-create table "image" (
+  create table "image"
+  (
     id uuid primary key default gen_random_uuid(),
     name varchar not null,
     path varchar not null,
     created timestamp default current_timestamp not null,
     modified timestamp default current_timestamp not null
-);
+  );
 
-create unique index unique_image_path on "image"(path);
+  create unique index unique_image_path on "image"(path);
 
-create type video_image_type_enum as enum ('thumbnail', 'chapter');
+  create type video_image_type_enum as enum
+  ('thumbnail', 'chapter');
 
-create table "video_image" (
+  create table "video_image"
+  (
     id uuid primary key default gen_random_uuid(),
     video_id uuid not null,
     image_id uuid not null,
@@ -58,25 +67,33 @@ create table "video_image" (
     modified timestamp default current_timestamp not null,
     constraint fk_video_image_video foreign key(video_id) references "video"(id) on delete cascade,
     constraint fk_video_image_image foreign key(image_id) references "image"(id) on delete cascade
-);
+  );
 
-create table "user" (
-  id uuid primary key default gen_random_uuid(),
-  username varchar not null, 
-  password varchar not null,
-  active boolean default true not null,
-  created timestamp default current_timestamp not null,
-  modified timestamp default current_timestamp not null
-);
+  create table "user"
+  (
+    id uuid primary key default gen_random_uuid(),
+    username varchar not null,
+    password varchar not null,
+    active boolean default true not null,
+    created timestamp default current_timestamp not null,
+    modified timestamp default current_timestamp not null
+  );
 
-create unique index unique_username_constraint on "user"(username);
+  create unique index unique_username_constraint on "user"(username);
 
-insert into "user" (username, password) values ('admin', '$2a$10$5nmh/cOu.dzk05V7lfBqQua9FO6nG.aQTGTJQFB26DGMSMwp5FWxu'); -- password = admin
+  insert into "user"
+    (username, password)
+  values
+    ('admin', '$2a$10$5nmh/cOu.dzk05V7lfBqQua9FO6nG.aQTGTJQFB26DGMSMwp5FWxu');
+  -- password = admin
 
-create type job_type_enum as enum ('update_existing_videos', 'scan_path','generate_checksum', 'generate_thumbnail');
-create type job_status_enum as enum ('not_started', 'in_progress', 'failed', 'completed', 'cancelled');
+  create type job_type_enum as enum
+  ('update_existing_videos', 'scan_path','generate_checksum', 'generate_thumbnail');
+  create type job_status_enum as enum
+  ('not_started', 'in_progress', 'failed', 'completed', 'cancelled');
 
-create table job (
+  create table job
+  (
     id uuid primary key default gen_random_uuid(),
     job_type job_type_enum not null,
     status job_status_enum not null,
@@ -84,4 +101,4 @@ create table job (
     outcome varchar,
     created timestamp default current_timestamp not null,
     modified timestamp default current_timestamp not null
-);
+  );
