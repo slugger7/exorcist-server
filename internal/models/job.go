23288322f2f981745b9fc6f02eeb62ccb7f00ada
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-05-08 19:42:00 +0200

    add priority to DTO and save to DB

--- internal/models/job.go
+++ internal/models/job.go
@@ -2,7 +2,8 @@ package models
 
 import "github.com/slugger7/exorcist/internal/db/exorcist/public/model"
 
-type CreateJob struct {
-	Type model.JobTypeEnum      `json:"type" binding:"required"`
-	Data map[string]interface{} `json:"data"`
+type CreateJobDTO struct {
+	Type     model.JobTypeEnum      `json:"type" binding:"required"`
+	Data     map[string]interface{} `json:"data"`
+	Priority *int16                 `json:"priority"`
 }
