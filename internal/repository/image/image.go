688ddb6c5af4e514bcae2ba0d245b4067ee91d85
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-05-26 15:35:05 +0200

    fix image repository

--- internal/repository/image/image.go
+++ internal/repository/image/image.go
@@ -3,7 +3,6 @@ package imageRepository
 import (
 	"context"
 	"database/sql"
-	"fmt"
 
 	"github.com/go-jet/jet/v2/postgres"
 	"github.com/google/uuid"
@@ -11,12 +10,17 @@ import (
 	"github.com/slugger7/exorcist/internal/db/exorcist/public/table"
 	"github.com/slugger7/exorcist/internal/environment"
 	errs "github.com/slugger7/exorcist/internal/errors"
+	"github.com/slugger7/exorcist/internal/repository/util"
 )
+
+type MediaImage struct {
+	model.Image
+	model.Media
+}
 
 type IImageRepository interface {
 	Create(m *model.Image) (*model.Image, error)
-	RelateVideo(m *model.VideoImage) (*model.VideoImage, error)
-	GetById(uuid.UUID) (*model.Image, error)
+	GetById(uuid.UUID) (*MediaImage, error)
 }
 
 type ImageRepository struct {
@@ -39,45 +43,33 @@ func New(db *sql.DB, env *environment.EnvironmentVariables, context context.Cont
 }
 
 func (i *ImageRepository) Create(m *model.Image) (*model.Image, error) {
-	var imgs []struct{ model.Image }
-	if err := i.createStatement(m).Query(&imgs); err != nil {
+	var img model.Image
+	statement := table.Image.INSERT(table.Image.AllColumns).
+		MODEL(m).
+		RETURNING(table.Image.AllColumns)
+
+	util.DebugCheck(i.Env, statement)
+
+	if err := statement.QueryContext(i.ctx, i.db, &img); err != nil {
 		return nil, errs.BuildError(err, "error creating image")
 	}
 
-	if len(imgs) == 1 {
-		return &imgs[len(imgs)-1].Image, nil
-	}
-
-	return nil, fmt.Errorf("no images were returned from query")
+	return &img, nil
 }
 
-func (i *ImageRepository) RelateVideo(m *model.VideoImage) (*model.VideoImage, error) {
-	var vidImgs []struct{ model.VideoImage }
-	if err := i.relateVideoStatement(m).Query(&vidImgs); err != nil {
-		return nil, errs.BuildError(err, "error creating relation between video and image")
-	}
-
-	if len(vidImgs) == 1 {
-		return &vidImgs[len(vidImgs)-1].VideoImage, nil
-	}
-
-	return nil, fmt.Errorf("no video image relations were returned from query")
-}
-
-func (i *ImageRepository) GetById(id uuid.UUID) (*model.Image, error) {
-	var imgs []struct{ model.Image }
-	statement := table.Image.SELECT(table.Image.AllColumns).
-		FROM(table.Image).
+func (i *ImageRepository) GetById(id uuid.UUID) (*MediaImage, error) {
+	var img MediaImage
+	statement := table.Image.SELECT(table.Image.AllColumns, table.Media.AllColumns).
+		FROM(table.Image.INNER_JOIN(
+			table.Image,
+			table.Image.MediaID.EQ(table.Media.ID),
+		)).
 		WHERE(table.Image.ID.EQ(postgres.UUID(id))).
 		LIMIT(1)
 
-	if err := statement.Query(i.db, &imgs); err != nil {
+	if err := statement.Query(i.db, &img); err != nil {
 		return nil, errs.BuildError(err, "error getting image by id")
 	}
 
-	if len(imgs) == 1 {
-		return &imgs[len(imgs)-1].Image, nil
-	}
-
-	return nil, fmt.Errorf("no images were returned for id: %v", id)
+	return &img, nil
 }
