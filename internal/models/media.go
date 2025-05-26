f62fc37f7f5f2cd7bc557933abf12db728965bfb
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-05-26 15:34:42 +0200

    create media ordinal type

--- internal/models/media.go
+++ internal/models/media.go
@@ -1,14 +1,47 @@
 package models
 
 import (
+	"github.com/go-jet/jet/v2/postgres"
 	"github.com/google/uuid"
 	"github.com/slugger7/exorcist/internal/db/exorcist/public/model"
+	"github.com/slugger7/exorcist/internal/db/exorcist/public/table"
 )
+
+type MediaOrdinal string
+
+const (
+	MediaOrdinal_Created  MediaOrdinal = "created"
+	MediaOrdinal_Modified MediaOrdinal = "modified"
+	MediaOrdinal_Path     MediaOrdinal = "path"
+	MediaOrdinal_Title    MediaOrdinal = "title"
+	MediaOrdinal_Size     MediaOrdinal = "size"
+	MediaOrdinal_Added    MediaOrdinal = "added"
+)
+
+func (o MediaOrdinal) ToColumn() postgres.Column {
+	media := table.Media
+	switch o {
+	case MediaOrdinal_Created:
+		return media.Created
+	case MediaOrdinal_Modified:
+		return media.Modified
+	case MediaOrdinal_Path:
+		return media.Path
+	case MediaOrdinal_Title:
+		return media.Title
+	case MediaOrdinal_Size:
+		return media.Size
+	case MediaOrdinal_Added:
+		return media.Added
+	default:
+		return media.Added
+	}
+}
 
 type MediaSearchDTO struct {
 	Limit   int          `form:"limit"`
 	Skip    int          `form:"skip"`
-	OrderBy VideoOrdinal `form:"orderBy"`
+	OrderBy MediaOrdinal `form:"orderBy"`
 	Asc     bool         `form:"asc"`
 	Search  string       `form:"search"`
 }
