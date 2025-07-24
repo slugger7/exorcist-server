c3607726b354d74520aa9a03f8f0353db73a0ade
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-07-24 14:40:03 +0200

    update media overview query to filter tags

--- internal/repository/helpers/media_overview_statement.go
+++ internal/repository/helpers/media_overview_statement.go
@@ -14,6 +14,7 @@ func MediaOverviewStatement(search dto.MediaSearchDTO, relationFn func(relationT
 	media := table.Media
 	mediaRelation := table.MediaRelation
 	thumbnail := table.Media.AS("thumbnail")
+	tag := table.Tag.AS("tag")
 	selectStatement := media.SELECT(
 		media.ID,
 		media.Title,
@@ -28,12 +29,22 @@ func MediaOverviewStatement(search dto.MediaSearchDTO, relationFn func(relationT
 				).LEFT_JOIN(
 					thumbnail,
 					thumbnail.ID.EQ(mediaRelation.RelatedTo),
-				))).
-		LIMIT(int64(search.Limit)).
-		OFFSET(int64(search.Skip))
+				).LEFT_JOIN(
+					table.MediaTag,
+					media.ID.EQ(table.MediaTag.MediaID),
+				).LEFT_JOIN(
+					tag,
+					table.MediaTag.TagID.EQ(table.Tag.ID),
+				)))
 
 	selectStatement = OrderByDirectionColumn(search.Asc, search.OrderBy.ToColumn(), selectStatement)
-	countStatement := media.SELECT(postgres.COUNT(media.ID).AS("total")).FROM(relationFn(media))
+	countStatement := media.SELECT(postgres.COUNT(media.ID).AS("total")).FROM(relationFn(media).LEFT_JOIN(
+		table.MediaTag,
+		media.ID.EQ(table.MediaTag.MediaID),
+	).LEFT_JOIN(
+		tag,
+		table.MediaTag.TagID.EQ(table.Tag.ID),
+	))
 
 	whr := media.MediaType.EQ(postgres.NewEnumValue(model.MediaTypeEnum_Primary.String())).
 		AND(media.Deleted.IS_FALSE()).
@@ -49,8 +60,30 @@ func MediaOverviewStatement(search dto.MediaSearchDTO, relationFn func(relationT
 			)
 	}
 
+	if len(search.Tags) > 0 {
+		tagExpressions := make([]postgres.Expression, len(search.Tags))
+		for i, t := range search.Tags {
+			tagExpressions[i] = postgres.String(t)
+		}
+
+		whr = whr.AND(
+			tag.Name.IN(tagExpressions...),
+		)
+	}
+
 	selectStatement = selectStatement.WHERE(whr)
 	countStatement = countStatement.WHERE(whr)
+
+	if len(search.Tags) > 0 {
+		selectStatement = selectStatement.GROUP_BY(media.ID, thumbnail.ID).
+			HAVING(postgres.COUNT(postgres.DISTINCT(tag.Name)).EQ(postgres.Int32(int32(len(search.Tags)))))
+		countStatement = countStatement.GROUP_BY(media.ID).
+			HAVING(postgres.COUNT(postgres.DISTINCT(tag.Name)).EQ(postgres.Int32(int32(len(search.Tags)))))
+	}
+
+	selectStatement = selectStatement.
+		LIMIT(int64(search.Limit)).
+		OFFSET(int64(search.Skip))
 
 	return selectStatement, countStatement
 }
