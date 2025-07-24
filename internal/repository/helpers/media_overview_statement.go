34f482fc58c281344907a16c3d96696dd4cf8753
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-07-24 14:50:01 +0200

    make query more reusable for next filter

--- internal/repository/helpers/media_overview_statement.go
+++ internal/repository/helpers/media_overview_statement.go
@@ -11,40 +11,48 @@ import (
 )
 
 func MediaOverviewStatement(search dto.MediaSearchDTO, relationFn func(relationTable postgres.ReadableTable) postgres.ReadableTable) (postgres.Statement, postgres.Statement) {
+	tagFilter := len(search.Tags) > 0
 	media := table.Media
 	mediaRelation := table.MediaRelation
 	thumbnail := table.Media.AS("thumbnail")
 	tag := table.Tag.AS("tag")
+
+	fromStmnt := relationFn(
+		media.LEFT_JOIN(
+			mediaRelation, media.ID.EQ(mediaRelation.MediaID).
+				AND(mediaRelation.RelationType.EQ(postgres.NewEnumValue(model.MediaRelationTypeEnum_Thumbnail.String()))),
+		).LEFT_JOIN(
+			thumbnail,
+			thumbnail.ID.EQ(mediaRelation.RelatedTo),
+		))
+
+	countFromStmnt := relationFn(media)
+
+	if tagFilter {
+		applyJoin := func(tbl postgres.ReadableTable) postgres.ReadableTable {
+			return tbl.LEFT_JOIN(
+				table.MediaTag,
+				media.ID.EQ(table.MediaTag.MediaID),
+			).LEFT_JOIN(
+				tag,
+				table.MediaTag.TagID.EQ(table.Tag.ID),
+			)
+		}
+		fromStmnt = applyJoin(fromStmnt)
+
+		countFromStmnt = applyJoin(countFromStmnt)
+	}
+
 	selectStatement := media.SELECT(
 		media.ID,
 		media.Title,
 		media.MediaType,
 		thumbnail.ID,
 	).
-		FROM(
-			relationFn(
-				media.LEFT_JOIN(
-					mediaRelation, media.ID.EQ(mediaRelation.MediaID).
-						AND(mediaRelation.RelationType.EQ(postgres.NewEnumValue(model.MediaRelationTypeEnum_Thumbnail.String()))),
-				).LEFT_JOIN(
-					thumbnail,
-					thumbnail.ID.EQ(mediaRelation.RelatedTo),
-				).LEFT_JOIN(
-					table.MediaTag,
-					media.ID.EQ(table.MediaTag.MediaID),
-				).LEFT_JOIN(
-					tag,
-					table.MediaTag.TagID.EQ(table.Tag.ID),
-				)))
+		FROM(fromStmnt)
 
 	selectStatement = OrderByDirectionColumn(search.Asc, search.OrderBy.ToColumn(), selectStatement)
-	countStatement := media.SELECT(postgres.COUNT(media.ID).AS("total")).FROM(relationFn(media).LEFT_JOIN(
-		table.MediaTag,
-		media.ID.EQ(table.MediaTag.MediaID),
-	).LEFT_JOIN(
-		tag,
-		table.MediaTag.TagID.EQ(table.Tag.ID),
-	))
+	countStatement := media.SELECT(postgres.COUNT(media.ID).AS("total")).FROM(countFromStmnt)
 
 	whr := media.MediaType.EQ(postgres.NewEnumValue(model.MediaTypeEnum_Primary.String())).
 		AND(media.Deleted.IS_FALSE()).
@@ -60,7 +68,7 @@ func MediaOverviewStatement(search dto.MediaSearchDTO, relationFn func(relationT
 			)
 	}
 
-	if len(search.Tags) > 0 {
+	if tagFilter {
 		tagExpressions := make([]postgres.Expression, len(search.Tags))
 		for i, t := range search.Tags {
 			tagExpressions[i] = postgres.String(t)
@@ -74,11 +82,14 @@ func MediaOverviewStatement(search dto.MediaSearchDTO, relationFn func(relationT
 	selectStatement = selectStatement.WHERE(whr)
 	countStatement = countStatement.WHERE(whr)
 
-	if len(search.Tags) > 0 {
-		selectStatement = selectStatement.GROUP_BY(media.ID, thumbnail.ID).
-			HAVING(postgres.COUNT(postgres.DISTINCT(tag.Name)).EQ(postgres.Int32(int32(len(search.Tags)))))
-		countStatement = countStatement.GROUP_BY(media.ID).
-			HAVING(postgres.COUNT(postgres.DISTINCT(tag.Name)).EQ(postgres.Int32(int32(len(search.Tags)))))
+	if tagFilter {
+		applyHaving := func(sl postgres.SelectStatement) postgres.SelectStatement {
+			return sl.HAVING(postgres.COUNT(postgres.DISTINCT(tag.Name)).EQ(postgres.Int32(int32(len(search.Tags)))))
+		}
+		selectStatement = applyHaving(selectStatement.GROUP_BY(media.ID, thumbnail.ID))
+
+		countStatement = applyHaving(countStatement.GROUP_BY(media.ID))
+
 	}
 
 	selectStatement = selectStatement.
