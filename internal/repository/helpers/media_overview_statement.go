dc5d0a6be19bd6a7cd735157634099f7535a7897
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-07-25 07:07:19 +0200

    add person filter to media overview search

--- internal/repository/helpers/media_overview_statement.go
+++ internal/repository/helpers/media_overview_statement.go
@@ -11,88 +11,129 @@ import (
 )
 
 func MediaOverviewStatement(search dto.MediaSearchDTO, relationFn func(relationTable postgres.ReadableTable) postgres.ReadableTable) (postgres.Statement, postgres.Statement) {
-	tagFilter := len(search.Tags) > 0
-	media := table.Media
-	mediaRelation := table.MediaRelation
-	thumbnail := table.Media.AS("thumbnail")
-	tag := table.Tag.AS("tag")
+	createSelect := func() postgres.SelectStatement {
+		tagFilter := len(search.Tags) > 0
+		personFilter := len(search.People) > 0
+		media := table.Media
+		mediaRelation := table.MediaRelation
+		thumbnail := table.Media.AS("thumbnail")
+		tag := table.Tag
 
-	fromStmnt := relationFn(
-		media.LEFT_JOIN(
-			mediaRelation, media.ID.EQ(mediaRelation.MediaID).
-				AND(mediaRelation.RelationType.EQ(postgres.NewEnumValue(model.MediaRelationTypeEnum_Thumbnail.String()))),
-		).LEFT_JOIN(
-			thumbnail,
-			thumbnail.ID.EQ(mediaRelation.RelatedTo),
-		))
-
-	countFromStmnt := relationFn(media)
-
-	if tagFilter {
-		applyJoin := func(tbl postgres.ReadableTable) postgres.ReadableTable {
-			return tbl.LEFT_JOIN(
-				table.MediaTag,
-				media.ID.EQ(table.MediaTag.MediaID),
+		fromStmnt := relationFn(
+			media.LEFT_JOIN(
+				mediaRelation, media.ID.EQ(mediaRelation.MediaID).
+					AND(mediaRelation.RelationType.EQ(postgres.NewEnumValue(model.MediaRelationTypeEnum_Thumbnail.String()))),
 			).LEFT_JOIN(
-				tag,
-				table.MediaTag.TagID.EQ(table.Tag.ID),
+				thumbnail,
+				thumbnail.ID.EQ(mediaRelation.RelatedTo),
+			))
+
+		countFromStmnt := relationFn(media)
+
+		if tagFilter {
+			applyJoin := func(tbl postgres.ReadableTable) postgres.ReadableTable {
+				return tbl.LEFT_JOIN(
+					table.MediaTag,
+					media.ID.EQ(table.MediaTag.MediaID),
+				).LEFT_JOIN(
+					tag,
+					table.MediaTag.TagID.EQ(table.Tag.ID),
+				)
+			}
+
+			fromStmnt = applyJoin(fromStmnt)
+			countFromStmnt = applyJoin(countFromStmnt)
+		}
+
+		if personFilter {
+			applyJoin := func(tbl postgres.ReadableTable) postgres.ReadableTable {
+				return tbl.LEFT_JOIN(
+					table.MediaPerson,
+					media.ID.EQ(table.MediaPerson.MediaID),
+				).LEFT_JOIN(
+					table.Person,
+					table.MediaPerson.PersonID.EQ(table.Person.ID),
+				)
+			}
+
+			fromStmnt = applyJoin(fromStmnt)
+			countFromStmnt = applyJoin(countFromStmnt)
+		}
+
+		selectStatement := media.SELECT(
+			media.ID,
+			media.Title,
+			media.MediaType,
+			thumbnail.ID,
+		).
+			FROM(fromStmnt)
+
+		selectStatement = OrderByDirectionColumn(search.Asc, search.OrderBy.ToColumn(), selectStatement)
+
+		whr := media.MediaType.EQ(postgres.NewEnumValue(model.MediaTypeEnum_Primary.String())).
+			AND(media.Deleted.IS_FALSE()).
+			AND(media.Exists.IS_TRUE())
+
+		if search.Search != "" {
+			caseInsensitive := strings.ToLower(search.Search)
+			likeExpression := fmt.Sprintf("%%%v%%", caseInsensitive)
+			whr = whr.
+				AND(
+					postgres.LOWER(media.Title).LIKE(postgres.String(likeExpression)).
+						OR(postgres.LOWER(media.Path).LIKE(postgres.String(likeExpression))),
+				)
+		}
+
+		if tagFilter {
+			tagExpressions := make([]postgres.Expression, len(search.Tags))
+			for i, t := range search.Tags {
+				tagExpressions[i] = postgres.String(t)
+			}
+
+			whr = whr.AND(
+				tag.Name.IN(tagExpressions...),
 			)
 		}
-		fromStmnt = applyJoin(fromStmnt)
 
-		countFromStmnt = applyJoin(countFromStmnt)
-	}
+		if personFilter {
+			peopleExpression := make([]postgres.Expression, len(search.People))
+			for i, p := range search.People {
+				peopleExpression[i] = postgres.String(p)
+			}
 
-	selectStatement := media.SELECT(
-		media.ID,
-		media.Title,
-		media.MediaType,
-		thumbnail.ID,
-	).
-		FROM(fromStmnt)
-
-	selectStatement = OrderByDirectionColumn(search.Asc, search.OrderBy.ToColumn(), selectStatement)
-	countStatement := media.SELECT(postgres.COUNT(media.ID).AS("total")).FROM(countFromStmnt)
-
-	whr := media.MediaType.EQ(postgres.NewEnumValue(model.MediaTypeEnum_Primary.String())).
-		AND(media.Deleted.IS_FALSE()).
-		AND(media.Exists.IS_TRUE())
-
-	if search.Search != "" {
-		caseInsensitive := strings.ToLower(search.Search)
-		likeExpression := fmt.Sprintf("%%%v%%", caseInsensitive)
-		whr = whr.
-			AND(
-				postgres.LOWER(media.Title).LIKE(postgres.String(likeExpression)).
-					OR(postgres.LOWER(media.Path).LIKE(postgres.String(likeExpression))),
+			whr = whr.AND(
+				table.Person.Name.IN(peopleExpression...),
 			)
-	}
-
-	if tagFilter {
-		tagExpressions := make([]postgres.Expression, len(search.Tags))
-		for i, t := range search.Tags {
-			tagExpressions[i] = postgres.String(t)
 		}
 
-		whr = whr.AND(
-			tag.Name.IN(tagExpressions...),
-		)
-	}
+		selectStatement = selectStatement.WHERE(whr)
 
-	selectStatement = selectStatement.WHERE(whr)
-	countStatement = countStatement.WHERE(whr)
-
-	if tagFilter {
-		applyHaving := func(sl postgres.SelectStatement) postgres.SelectStatement {
-			return sl.HAVING(postgres.COUNT(postgres.DISTINCT(tag.Name)).EQ(postgres.Int32(int32(len(search.Tags)))))
+		if tagFilter || personFilter {
+			selectStatement = selectStatement.GROUP_BY(media.ID, thumbnail.ID)
 		}
-		selectStatement = applyHaving(selectStatement.GROUP_BY(media.ID, thumbnail.ID))
 
-		countStatement = applyHaving(countStatement.GROUP_BY(media.ID))
+		if tagFilter {
+			applyHaving := func(sl postgres.SelectStatement) postgres.SelectStatement {
+				return sl.HAVING(postgres.COUNT(postgres.DISTINCT(tag.Name)).EQ(postgres.Int32(int32(len(search.Tags)))))
+			}
 
+			selectStatement = applyHaving(selectStatement)
+		}
+
+		if personFilter {
+			applyHaving := func(sl postgres.SelectStatement) postgres.SelectStatement {
+				return sl.HAVING(postgres.COUNT(postgres.DISTINCT(table.Person.Name)).EQ(postgres.Int32(int32(len(search.People)))))
+			}
+
+			selectStatement = applyHaving(selectStatement)
+		}
+
+		return selectStatement
 	}
 
-	selectStatement = selectStatement.
+	countStatement := createSelect().AsTable("media_overview").SELECT(postgres.COUNT(postgres.String("id")).AS("total"))
+
+	selectStatement := createSelect().
 		LIMIT(int64(search.Limit)).
 		OFFSET(int64(search.Skip))
 
