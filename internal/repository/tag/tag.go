a3451dbdd01a45249be7d870c66bee080cab2b30
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-07-28 16:15:35 +0200

    media overview by tag use new count query

--- internal/repository/tag/tag.go
+++ internal/repository/tag/tag.go
@@ -55,28 +55,32 @@ func (r *tagRepository) GetMedia(id uuid.UUID, search dto.MediaSearchDTO) (*dto.
 		return whr.AND(mediaTag.TagID.EQ(postgres.UUID(id)))
 	}
 
-	selectStatement, countStatement := helpers.MediaOverviewStatement(search, relationFn, whereFn)
+	selectStatement := helpers.MediaOverviewStatement(search, relationFn, whereFn)
 
-	util.DebugCheck(r.env, countStatement)
 	util.DebugCheck(r.env, selectStatement)
 
-	var total struct {
+	var mediaResult []struct {
 		Total int
+		models.MediaOverviewModel
 	}
-	if err := countStatement.QueryContext(r.ctx, r.db, &total); err != nil {
-		return nil, errs.BuildError(err, "could not query media total by tag id: %v", id.String())
-	}
-
-	var mediaResult []models.MediaOverviewModel
 	if err := selectStatement.QueryContext(r.ctx, r.db, &mediaResult); err != nil {
 		return nil, errs.BuildError(err, "could not query media by tag id: %v", id.String())
 	}
 
+	data := make([]models.MediaOverviewModel, len(mediaResult))
+	total := 0
+	if mediaResult != nil && len(mediaResult) > 0 {
+		total = mediaResult[0].Total
+		for i, o := range mediaResult {
+			data[i] = o.MediaOverviewModel
+		}
+	}
+
 	return &dto.PageDTO[models.MediaOverviewModel]{
-		Data:  mediaResult,
+		Data:  data,
 		Limit: search.Limit,
 		Skip:  search.Skip,
-		Total: total.Total,
+		Total: total,
 	}, nil
 }
 
