9d45386ec5aa17aaf85eef647d0386cf47c66681
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-07-30 12:02:01 +0200

    remove duplicates from media progress before altering table

--- migrations/20250729055945_media-progress-unique-columns.up.sql
+++ migrations/20250729055945_media-progress-unique-columns.up.sql
@@ -1,1 +1,11 @@
-alter table media_progress add constraint uq_media_progress_media_id_user_id unique (media_id, user_id)
+with duplicate_progress as (select id,
+        row_number() over (partition by user_id, media_id order by timestamp desc) as rn
+from media_progress)
+delete from media_progress
+where id in (
+  select id
+  from duplicate_progress 
+  where duplicate_progress.rn > 1
+);
+
+alter table media_progress add constraint uq_media_progress_media_id_user_id unique (media_id, user_id);
