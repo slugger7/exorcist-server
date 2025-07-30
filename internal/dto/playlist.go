64f9ff61181a78c7dc159226e85b45e4b3e3b8cb
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-07-30 15:38:00 +0200

    create playlist dtos

---
+++ internal/dto/playlist.go
@@ -0,0 +1,28 @@
+package dto
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/slugger7/exorcist/internal/db/exorcist/public/model"
+)
+
+type CreatePlaylistDTO struct {
+	Name string `json:"string"`
+}
+
+type PlaylistDTO struct {
+	ID       uuid.UUID `json:"id"`
+	Name     string    `json:"name"`
+	Created  time.Time `json:"created"`
+	Modified time.Time `json:"modified"`
+}
+
+func (d *PlaylistDTO) FromModel(m model.Playlist) *PlaylistDTO {
+	d.ID = m.ID
+	d.Name = m.Name
+	d.Created = m.Created
+	d.Modified = m.Modified
+
+	return d
+}
