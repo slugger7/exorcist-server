4eec11b4a8df961f7fa6a18d17e3f3ad3fe384f0
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-07-30 15:37:27 +0200

    create all playlists repository method

--- internal/repository/playlist/playlist.go
+++ internal/repository/playlist/playlist.go
@@ -12,12 +12,27 @@ import (
 
 type PlaylistRepository interface {
 	GetAll() ([]model.Playlist, error)
+	CreateAll(playlists []model.Playlist) ([]model.Playlist, error)
 }
 
 type playlistRepository struct {
 	env *environment.EnvironmentVariables
 	db  *sql.DB
 	ctx context.Context
+}
+
+// CreateAll implements PlaylistRepository.
+func (p *playlistRepository) CreateAll(playlists []model.Playlist) ([]model.Playlist, error) {
+	statement := table.Playlist.INSERT(table.Playlist.UserID, table.Playlist.Name).
+		MODELS(playlists).
+		RETURNING(table.Playlist.AllColumns)
+
+	var playlistEntities []model.Playlist
+	if err := statement.QueryContext(p.ctx, p.db, &playlistEntities); err != nil {
+		return nil, errs.BuildError(err, "could not create and return playlists")
+	}
+
+	return playlistEntities, nil
 }
 
 // GetAll implements PlaylistRepository.
